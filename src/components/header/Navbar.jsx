6cf3d813d434b6cb8ca0ee1b5157eea4d1910f23
Akmal Ihksan <akmal8149@smk.belajar.id>
2024-10-18 21:41:45 +0700

    Update Navbar.jsx

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,10 +1,10 @@
 import {
-  faBars,
-  faBolt,
-  faCalendar,
-  faHouse,
-  faRightFromBracket,
-  faUser,
+    faBars,
+    faBolt,
+    faCalendar,
+    faHouse,
+    faRightFromBracket,
+    faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
@@ -13,92 +13,68 @@ import { Link, useNavigate } from "react-router-dom";
 import { logOut } from "../../utils/authUtils";
 
 const Menu = () => {
-  const [logout, setLogout] = useState(true);
-  const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
 
-  function handleLogout() {
-    logOut();
-    navigate("/");
-  }
+    useEffect(() => {
+        const status = localStorage.getItem("isLoggedIn");
+        setIsLoggedIn(!!status);
+    }, []);
 
-  function isLogout() {
-    const isLoggedIn = localStorage.getItem("isLoggedIn");
-    if (isLoggedIn) {
-      setLogout(!logout);
-    }
-  }
+    const handleLogout = () => {
+        logOut();
+        navigate("/");
+    };
 
-  useEffect(() => {
-    isLogout();
-  }, []);
-
-  return (
-    <>
-      <nav className="menu-container">
-        <ul className="menu-list-container">
-          <li className="menu-list">
-            <FontAwesomeIcon icon={faCalendar} />
-            <Link className="menu-link" to="/todos">
-              Todos
-            </Link>
-          </li>
-          <li className="menu-list">
-            <FontAwesomeIcon icon={faUser} />
-            <Link className="menu-link" to="/profile">
-              Profile
-            </Link>
-          </li>
-          <li className="menu-list">
-            <FontAwesomeIcon icon={faHouse} />
-            <Link className="menu-link" to="/">
-              Home
-            </Link>
-          </li>
-        </ul>
-        <div className="logout">
-          {logout ? null : (
-            <>
-              <FontAwesomeIcon icon={faRightFromBracket} />
-              <Link onClick={handleLogout} className="logout-link">
-                Logout
-              </Link>
-            </>
-          )}
-        </div>
-      </nav>
-    </>
-  );
+    return (
+        <nav className="menu-container">
+            <ul className="menu-list-container">
+                <li className="menu-list">
+                    <FontAwesomeIcon icon={faCalendar} />
+                    <Link className="menu-link" to="/todos">Todos</Link>
+                </li>
+                <li className="menu-list">
+                    <FontAwesomeIcon icon={faUser} />
+                    <Link className="menu-link" to="/profile">Profile</Link>
+                </li>
+                <li className="menu-list">
+                    <FontAwesomeIcon icon={faHouse} />
+                    <Link className="menu-link" to="/">Home</Link>
+                </li>
+            </ul>
+            {isLoggedIn && (
+                <div className="logout">
+                    <FontAwesomeIcon icon={faRightFromBracket} />
+                    <Link onClick={handleLogout} className="logout-link">Logout</Link>
+                </div>
+            )}
+        </nav>
+    );
 };
 
 const Navbar = () => {
-  const [menu, setMenu] = useState(false);
+    const [menu, setMenu] = useState(false);
 
-  function handleMenu() {
-    setMenu(!menu);
-  }
+    const handleMenuToggle = () => {
+        setMenu(prevMenu => !prevMenu);
+    };
 
-  return (
-    <>
-      <div className="nav-container">
-        <div className="nav-logo">
-          <FontAwesomeIcon className="logos" icon={faBolt} />
-          <h4 className="main-title">SanNotes.</h4>
-        </div>
-        <div onClick={handleMenu} className="hamburger-menu">
-          <FontAwesomeIcon icon={faBars} />
-        </div>
-      </div>
-      {menu ? (
-        <div className="menu-on">
-          <Menu />
-        </div>
-      ) : (
-        <div className="menu-off">
-          <Menu />
-        </div>
-      )}
-    </>
-  );
+    return (
+        <>
+            <div className="nav-container">
+                <div className="nav-logo">
+                    <FontAwesomeIcon className="logos" icon={faBolt} />
+                    <h4 className="main-title">SanNotes.</h4>
+                </div>
+                <div onClick={handleMenuToggle} className="hamburger-menu">
+                    <FontAwesomeIcon icon={faBars} />
+                </div>
+            </div>
+            <div className={menu ? "menu-on" : "menu-off"}>
+                <Menu />
+            </div>
+        </>
+    );
 };
 
 export default Navbar;
